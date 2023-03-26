b0b8f27260a2e69ca9a081ece74b9276aa18b334
Ryoumc <2530980416@qq.com>
2023-03-26 17:24:46 +0800

    test

---
+++ src/components/Dailog-element.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="dailog">
+    <el-dialog title="收货地址" :visible.sync="dialogVisible" :close="close" :before-close="handleClose">
+      <el-table :data="gridData">
+        <el-table-column
+          property="date"
+          label="日期"
+          width="150"
+        ></el-table-column>
+        <el-table-column
+          property="name"
+          label="姓名"
+          width="200"
+        ></el-table-column>
+        <el-table-column property="address" label="地址"></el-table-column>
+      </el-table>
+      <el-button @click="dialogVisible = false">取 消</el-button>
+      <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    dialogVisible: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      gridData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+      ],
+    };
+  },
+  methods: {
+    handleClose(done) {
+      this.$confirm("确认关闭？")
+        .then((_) => {
+          done();
+        })
+        .catch((_) => {});
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <button type="text" @click="dialogVisible =!dialogVisible ">dailog</button>
+    <Dailog :dialogVisible="dialogVisible"></Dailog>
+  </div>
+</template>
+
+<script>
+
+import Dailog from '@/components/Dailog-element.vue'
+export default {
+  components: {
+    Dailog,
+  },
+  data(){
+    return{
+      dialogVisible: false,
+    }
+  }
+}
+</script>
